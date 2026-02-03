cf26d8943958b6b4718cc1ada4b1df9ac390d766
icris <crissaleta09@gmail.com>
2026-02-03 21:14:10 +0100

    Add initial project structure with Spring Boot setup and configuration files

---
+++ src/main/java/com/pannermanagement/PlannerManagement/PlannerManagementApplication.java
@@ -0,0 +1,13 @@
+package com.pannermanagement.PlannerManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pannermanagement/PlannerManagement/PlannerManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pannermanagement.PlannerManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
